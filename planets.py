c89d54827515f202b99f17fe473390ee99a0e2ff
etdeg16 <ethandguzman@gmail.com>
2018-09-22 18:18:15 -0700

    Added comments at top of file

--- planets.py
+++ planets.py
@@ -1,3 +1,12 @@
+#
+#Name; Ethan De Guzman
+#Student ID: 012820349
+#Date (last modified): 9/22/2018
+#
+#Lab 0
+#Section 16
+#Purpose of lab: To get comfortable programming in a Python enivornment and with testing.
+
 def weight_on_planets():
     earth_weight = float(input("What do you weigh on earth? "))
     mars_weight = earth_weight * 0.38
